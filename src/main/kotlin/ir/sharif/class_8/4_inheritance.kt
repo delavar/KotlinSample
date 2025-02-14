bc58b27e0a0272905349c7dd2f3be38efd0c46e6
Alireza Delavar <alireza.delavar@divar.ir>
2025-02-14 23:45:06 +0330

    add inheritance

---
+++ src/main/kotlin/ir/sharif/class_8/4_inheritance.kt
@@ -0,0 +1,30 @@
+package ir.sharif.class_8.data_class
+
+interface IShape {
+    val vertexCount: Int
+    fun draw()
+}
+
+class Polygon : IShape {
+    override var vertexCount: Int = 0  // Can be set to any number later
+    override fun draw() {
+        println("Drawing a polygon")
+    }
+}
+
+// Always has 4 vertices
+open class Rectangle(override val vertexCount: Int = 4) : IShape {
+    open val filledColor : String = "transparent"
+    override fun draw() {
+        println("Drawing a rectangle")
+    }
+}
+
+
+class FilledRectangle : Rectangle() {
+    override val filledColor: String = "black"
+    override fun draw() {
+        super.draw()
+        println("Filling the rectangle")
+    }
+}
