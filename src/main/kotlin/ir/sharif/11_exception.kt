694cd2e7f8ccde960d905888d014d5e45fe65210
Alireza Delavar <alireza.delavar@divar.ir>
2025-02-15 00:32:20 +0330

    add try catch

---
+++ src/main/kotlin/ir/sharif/11_exception.kt
@@ -0,0 +1,26 @@
+package ir.sharif
+
+
+//Kotlin treats all exceptions as unchecked by default
+
+
+fun getIndices(count: Int): List<Int> {
+    /*if(count < 0) {
+        throw IllegalArgumentException("Count must be non-negative. You set count to $count.")
+    }*/
+    require(count >= 0) { "Count must be non-negative. You set count to $count." }
+    return List(count) { it + 1 }
+}
+
+fun main() {
+    // This fails with an IllegalArgumentException
+    try {
+        println(getIndices(-1))
+    }catch (e: IllegalArgumentException){
+        println("error")
+    }
+
+    // Uncomment the line below to see a working example
+    // println(getIndices(3))
+    // [1, 2, 3]
+}
